fd4c29469fad1626494dfc32c8a94a1bbad06cf8
Jaspinder Singh <jaspindersingh@Jaspinders-MacBook-Pro.local>
2026-06-14 10:06:10 +0530

    intro to search agent

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-from typing import List
-
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+# from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
+# tavily = TavilyClient()
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
+# @tool
+# def search(query : str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search results
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
-    url: str = Field(description="The URL of the source")
-
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
-
-
-llm = ChatOpenAI(model="gpt-5")
+llm = ChatOllama(model="llama3.2:latest", temperature=0)
+# tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
-
+agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
-        }
-    )
+    result = agent.invoke({"messages": [HumanMessage(content="Search for 3 recent job postings on Linkedin for AI engineer in Bangalore ?")]})
     print(result)
-
 
 if __name__ == "__main__":
     main()

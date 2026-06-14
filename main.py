e196c6247f25932268979140959edc11b9d26cde
Jaspinder Singh <jaspindersingh@Jaspinders-MacBook-Pro.local>
2026-06-14 10:57:34 +0530

    structed response with langchain and pydantic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from dotenv import load_dotenv
+from typing import List
 
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 load_dotenv(override=True)
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -22,10 +24,28 @@ from langchain_tavily import TavilySearch
 #     print(f"Searching for {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The url of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answers")
+
 llm = ChatOllama(model="llama3.2:latest", temperature=0)
 # tools = [search]
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(
+    model=llm,
+    tools=tools,
+    response_format=AgentResponse,
+    system_prompt=("""You are a research assistant. Use TavilySearch to find information.
+        When you have enough information, you MUST respond by calling the 
+        AgentResponse tool with your final answer and source URLs. 
+        Do NOT reply with plain text as your final answer."""))
 
 def main():
     print("Hello from langchain-course!")
